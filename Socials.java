1701da8d222938459247a97281aea0af5d7b4c60
Tristan-Thompson876 <125337721+Tristan-Thompson876@users.noreply.github.com>
2023-10-31 03:31:37 -0500

    ..

---
+++ Socials.java
@@ -0,0 +1,14 @@
+public class Socials{
+    String name;
+    //FeedAlgorithm feedSort;
+    int pstID;
+
+    public void addNewSocialsPost(Post pst){
+
+    }
+    public
+
+
+
+
+}
--- User.java
+++ User.java
@@ -1,3 +1,10 @@
 public class User {
-    
+    String username;
+    String password;
+
+    public Boolean login(String username, String pword){
+        return null;
+    }
+
+    public addNewPost
 }
