f09b5b19f2b13597168468578920e6230150ef54
Tristan-Thompson876 <125337721+Tristan-Thompson876@users.noreply.github.com>
2023-10-31 04:14:21 -0500

    yess

--- Socials.java
+++ Socials.java
@@ -2,12 +2,51 @@ public class Socials{
     String name;
     //FeedAlgorithm feedSort;
     int pstID;
+    public Socials(){
+
+    }
 
     public void addNewSocialsPost(Post pst){
 
     }
-    public
 
+    public void removeSocialsPost(int pstID){
+
+    }
+    public Boolean searchForSocialsPost(Post pst){
+        return null;
+    }
+
+    public String whoOwnsSocialsPost(int pstID){
+        return null;
+    }
+
+    public Integer getAllSocialsPosts(){
+        return null;
+    }
+    public void addNewSocialsUser(String name, String password){
+
+    }
+
+    public void removeSocialsUser(String name){
+
+    }
+
+    public Boolean isSocialsUser(String name){
+        return null;
+    }
+
+    public String getAllSocialsUsers(){
+        return null;
+    }
+
+    public Integer getSocialsUserFeed(String name){
+        return null;
+    }
+
+    public String toString(){
+        return null;
+    }
 
 
 
--- User.java
+++ User.java
@@ -6,5 +6,46 @@ public class User {
         return null;
     }
 
-    public addNewPost
+    //public addNewPost()
+    public void deletePost(int pstID){
+
+    }
+    public void reactToPost(String pstID, ReactionType vote){
+        
+    }
+    public Boolean subscribe(String name){
+        return null;
+    }
+    public Boolean unsubscribe(String name){
+        return null;
+    }
+    public Boolean restrict(String name){
+        return null;
+
+    }
+    public Boolean unrestrict(String name){
+        return null;
+    }
+    public Boolean isASubscriber(String name){
+        return null;
+    }
+    public Boolean isSubscribed(String name){
+        return null;
+    }
+    public Boolean isRestricted(String name){
+        return null;
+    }
+    public Boolean hasAccessToPost(int pstID){
+        return null;
+    }
+    public Boolean isPostOwner(int pstID){
+        return null;
+    }
+    public String subscribersWithAccessToPost(int pstID){
+        return null;
+    }
+    public String toString(){
+        return null;
+    }
+
 }
