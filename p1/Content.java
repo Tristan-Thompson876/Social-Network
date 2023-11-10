141ce9f8c2cdf04c52ed0f3d12a0946c1b719aba
Kyval Waysome <waysomekyval@gmail.com>
2023-11-10 07:38:42 -0800

    Updated Content again

--- p1/Content.java
+++ p1/Content.java
@@ -1,37 +1,17 @@
 package p1;
 
-/**
- * class Content
- */
 public class Content {
-    private String data;
+	// immutable instance data
+	private String data;
 
-    /**
-     * Constructor for Content
-     * @param data
-     */
-    public Content(String data) {
-        this.data = data;
-    }
+	public Content(String data) {
+		super();
+		this.data = data;
+	}
 
-    /**
-     * 
-     * @return String data
-     */
-    public String getData() {
-        return data;
-    }
+	@Override
+	public String toString() {
+		return ""+this.data;
+	}
 
-    /**
-     * 
-     * @param data
-     */
-    public void setData(String data) {
-        this.data = data;
-    }
-
-    @Override
-    public String toString() {
-        return "Content:" + data + '\'';
-    }
 }
